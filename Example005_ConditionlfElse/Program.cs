f6e97514db33cbd92d985da9456efed5e70fb280
Aleksandra <igoshina.sashenka98@gmail.com>
2023-03-15 15:25:41 +0700

    5

---
+++ Example005_ConditionlfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower == "Маша") // если имя пользователя равен Маша, ToLower - нижний регистр
+{
+    Console.WriteLine("Ура, это же Маша!");  //Консоль выводит Машу  
+}
+else // иначе
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
