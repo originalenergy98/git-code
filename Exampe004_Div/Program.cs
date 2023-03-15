26a53e08ed64e895c1e81c439dc79102e4d735f3
Aleksandra <igoshina.sashenka98@gmail.com>
2023-03-15 14:59:43 +0700

    4

---
+++ Exampe004_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+// типы данных:
+// int - от -2 147 483 648 до 2 147 483 647
+// double - от +- 5,0 х 10^-324 до +-1,7 х 10^308
+// string - от 0 до букв и разных символов
+// bool - true и false
