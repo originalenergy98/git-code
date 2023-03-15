961c5a9c06c0691644da9c0f28f76bae0003c62c
Aleksandra <igoshina.sashenka98@gmail.com>
2023-03-15 14:54:47 +0700

    2

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,13 @@
+﻿// Приветствуем пользователя
+
+Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine(); // сохранить в строку = user name
+//ReadLine - запрос из терминала
+// string - строка/слова = вид  информации
+// username username - коробка
+// username - название коробки
+Console.Write("Привет, " + username);
+
+// Write(); - Вывод в одну строку
+// WriteLine(); - В конце перейти на новую  строку
+// ReadLine(); - Считатьстроку из терммнала
